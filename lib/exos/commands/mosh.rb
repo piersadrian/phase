5f68b3765f85923a68849ec02dc90fb3407ec10e
Piers Mainwaring <piers@impossibly.org>
2014-10-20 14:53:06 -0600

    Fixed mosh implementation.

--- lib/exos/commands/mosh.rb
+++ lib/exos/commands/mosh.rb
@@ -1,10 +1,23 @@
 module Exos
   module Commands
     class Mosh < SSH
-      def run
-        options.default conn: "mosh"
-        super
+
+      command :mosh do |c|
+        c.syntax = "exos mosh [-i instance_id] [-n instance_name] [-r instance_role] [-u user] [-c conn_str] [username@instance_name|instance_id]"
+
+        c.option "-i", "--id instance_id", String, "Connects to the instance with this ID."
+        c.option "-n", "--name instance_name", String, "Connects to the instance with this 'Name' tag."
+        c.option "-r", "--role instance_role", String, "Connects to an instance with this 'Role' tag. Default is 'ssh'."
+        c.option "-u", "--user username", String, "Remote username to connect with."
+        c.option "-c", "--conn conn_str", String, "Invokes conn_str to establish terminal session (e.g. --ssh 'ssh -i key.pem')."
+
+        c.description = "Connects to the the specified instance via mosh."
+        c.action do |args, options|
+          options.default role: "ssh", conn: "mosh"
+          new(args, options).run
+        end
       end
+
     end
   end
 end
--- lib/exos/commands/ssh.rb
+++ lib/exos/commands/ssh.rb
@@ -3,7 +3,7 @@ module Exos
     class SSH < Command
 
       command :ssh do |c|
-        c.syntax = "exos ssh [-i instance_id] [-n instance_name] [-r instance_role] [-u user] [username@instance_name|instance_id]"
+        c.syntax = "exos ssh [-i instance_id] [-n instance_name] [-r instance_role] [-u user] [-c conn_str] [username@instance_name|instance_id]"
 
         c.option "-i", "--id instance_id", String, "Connects to the instance with this ID."
         c.option "-n", "--name instance_name", String, "Connects to the instance with this 'Name' tag."
@@ -18,34 +18,45 @@ module Exos
         end
       end
 
+      attr_accessor :user, :instance
+
       def run
-        # Handle "connection string" style parameter.
-        if conn = args.first
-          user, str = conn.split("@")
-
-          fail "Malformed parameter: user@[instance-name|instance-id]." if str.nil?
-
-          str.match(/i-[0-9a-f]+/i) ? options.id = str : options.name = str
-        else
-          user = options.user
-        end
-
-        fail "Must specify username with -u or 'user@instance-name' parameter." if user.nil?
-
-        if options.id
-          instance = ec2.servers.all("instance-id" => options.id).first
-        elsif options.name
-          instance = ec2.servers.all("tag:Name" => options.name).first
-        else
-          instance = ec2.servers.all("tag:Role" => options.role).first
-        end
-
-        fail "No instance found. Exiting." if instance.nil?
-
-        log "connecting to instance #{ instance.id } via SSH..."
-        connection_cmd = "#{ options.conn } #{ user }@#{ instance.dns_name }"
-        exec connection_cmd
+        log "connecting to instance #{ instance.id }..."
+        exec "#{ options.conn } #{ user }@#{ instance.dns_name }"
       end
+
+      private
+
+        def user
+          # Handle "connection string" style parameter.
+          if conn = args.first
+            user, str = conn.split("@")
+
+            fail "Malformed parameter: user@[instance-name|instance-id]." if str.nil?
+
+            str.match(/i-[0-9a-f]+/i) ? options.id = str : options.name = str
+          else
+            user = options.user
+          end
+
+          fail "Must specify username with -u or 'user@instance-name' parameter." if user.nil?
+
+          user
+        end
+
+        def instance
+          if options.id
+            instance = ec2.servers.all("instance-id" => options.id).first
+          elsif options.name
+            instance = ec2.servers.all("tag:Name" => options.name).first
+          else
+            instance = ec2.servers.all("tag:Role" => options.role).first
+          end
+
+          fail "no instance found." if instance.nil?
+
+          instance
+        end
 
     end
   end
